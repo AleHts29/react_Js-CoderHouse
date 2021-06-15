26c50cdcc0ded5ae6b0abd91ac60c76d86355007
AleHts29 <ahuertas@fi.uba.ar>
2021-06-14 22:12:58 -0300

    clase03

---
+++ clase2-app/src/componets/navbarComponents/Index.jsx
@@ -0,0 +1,15 @@
+// atajo de template 
+//     rfce/rafce
+
+
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar;
